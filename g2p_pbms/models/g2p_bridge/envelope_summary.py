f260698e16fb63f65300ff5f7de645ee2dfcf37b
tahzeer <tahzeer.ash@gmail.com>
2025-06-24 09:35:00 +0530

    fix: handle missing 'message' in API response for disbursement envelope summary

--- g2p_pbms/models/g2p_bridge/envelope_summary.py
+++ g2p_pbms/models/g2p_bridge/envelope_summary.py
@@ -83,7 +83,9 @@ class G2PDisbursementEnvelopeSummaryWizard(models.TransientModel):
             return
 
         message = api_response.get('message', {})
-        # TODO: check if message is None
+        if not message:
+            _logger.error("No 'message' found in API response: %s", api_response)
+            return
         self.disbursement_envelope_id = message.get('disbursement_envelope_id', self.disbursement_envelope_id)
         self.number_of_disbursements_received = message.get('number_of_disbursements_received', 0)
         self.total_disbursement_amount_received = f"{self.measurement_unit} {format(float(message.get('total_disbursement_amount_received', 0)), ',')}"
