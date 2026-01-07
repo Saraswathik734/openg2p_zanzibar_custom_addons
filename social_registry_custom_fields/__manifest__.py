a949205ecc3be9d10329baa5c09b7c0f5a2cecfe
RohailAkthar <aktharohail@gmail.com>
2026-01-07 13:31:20 +0530

    new module

---
+++ social_registry_custom_fields/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Social Registry Custom Fields",
+    "version": "17.0.1.0.0",
+    "category": "G2P",
+    "summary": "Custom fields for Social Registry",
+    "author": "Admin",
+    "website": "https://openg2p.org",
+    "license": "LGPL-3",
+    "depends": [
+        "g2p_social_registry",
+        "g2p_registry_individual",
+    ],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
---
+++ social_registry_custom_fields/models/res_partner.py
@@ -0,0 +1,18 @@
+from odoo import models, fields
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    benf_post_code = fields.Char(string="Post Code")
+    benf_zan_id = fields.Char(string="Zan ID")
+    disability = fields.Selection(
+        [("yes", "Yes"), ("no", "No")], string="Do you have any disability?"
+    )
+    is_receiving_allowance = fields.Selection(
+        [("yes", "Yes"), ("no", "No")],
+        string="Are you receiving 5000 allowance from district council? (Below 70 years)",
+    )
+    has_health_insurance = fields.Selection(
+        [("yes", "Yes"), ("no", "No")],
+        string="Are you covered with any health insurance scheme?",
+    )
