42c2309f823ddf7f0359e7031ece61e4a5db256f
Saraswathi K <saraswathi.k@cyberpwn.com>
2026-02-13 17:18:17 +0530

    Add hide_partner_lang module

---
+++ hide_partner_lang/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Hide Language Field – Individual Registrant",
+    "version": "1.0",
+    "depends": ["g2p_registry_individual"],
+    "data": [
+        "views/res_partner_view.xml",
+    ],
+    "installable": True,
+}
